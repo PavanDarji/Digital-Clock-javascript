b4656906aa83a2c0a52dc6617deddc982198edc5
Pavan Darji <84569753+PavanDarji@users.noreply.github.com>
2021-07-08 03:56:20 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function showTime() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minit = date.getMinutes();
+    let second = date.getSeconds();
+
+    let session = 'AM';
+    if (hours == 0) {
+        hours = 12;
+    }
+    if (hours > 12) {
+        hours = hours - 12;
+        session = 'PM';
+    }
+
+
+    hours = (hours < 10) ? '0' + hours : hours;
+    minit = (minit < 10) ? '0' + minit : minit;
+    second = (second < 10) ? '0' + second : second;
+
+
+    let time = hours + ':' + minit + ':' + second + ' ' + session;
+
+    document.querySelector('#myTime').innerText = time;
+
+    setTimeout(showTime, 1000);
+
+
+}
+
+showTime();
