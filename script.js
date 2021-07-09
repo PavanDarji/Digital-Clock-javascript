a1410b4d7c67af19f57d350a4433afc08f727f28
Pavan Darji <84569753+PavanDarji@users.noreply.github.com>
2021-07-09 02:01:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ function showTime() {
     let hours = date.getHours();
     let minit = date.getMinutes();
     let second = date.getSeconds();
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+
+    
 
     let session = 'AM';
     if (hours == 0) {
@@ -17,11 +22,14 @@ function showTime() {
     hours = (hours < 10) ? '0' + hours : hours;
     minit = (minit < 10) ? '0' + minit : minit;
     second = (second < 10) ? '0' + second : second;
+    
 
-
-    let time = hours + ':' + minit + ':' + second + ' ' + session;
+    let time = hours + ':' + minit + ':' + second + ' ' + session; 
+    let result = day + ' / ' + month + ' / ' + year; 
+    
 
     document.querySelector('#myTime').innerText = time;
+    document.querySelector('#myDay').innerText = result;
 
     setTimeout(showTime, 1000);
 
